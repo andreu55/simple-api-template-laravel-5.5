f477addeb121bffe403e6c13d89fa26c67e94e94
Andreu García <anduwet2@gmail.com>
2018-09-22 13:17:15 +0200

    - Gestion de excepciones jsoned

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -5,6 +5,8 @@ namespace App\Exceptions;
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
+use Illuminate\Auth\AuthenticationException;
+
 class Handler extends ExceptionHandler
 {
     /**
@@ -48,6 +50,15 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        return parent::render($request, $exception);
+        if (env('APP_DEBUG', true)) {
+          return parent::render($request, $exception);
+        } else {
+          return response()->json(['error' => $exception->getMessage()], 401);
+        }
+    }
+
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        return response()->json(['error' => 'Token no válido (' . $exception->getMessage() . ')'], 401);
     }
 }
